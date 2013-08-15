ffd337b54a57ae771d0d4090b33fa1fd8831ff39
James Trinklein <jtrinklein@daptiv.com>
2013-08-15 13:09:05 -0700

    return object status

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ var express = require('express'),
 	app = express();
 var status = [
 {
-	name: 'Status',
-	stamp: 'Last Update 7/17 by Robin Sherwood',
-	fields: [
-		{ label: 'Status Update', type: 'longtext', value: 'Completed phase two check point agreed to move to phase three pending the clusure of the current open issues. Planned finish date was adjuested by one week to acoomodate for the end of quarter.' },
-		{label: 'Health', type: 'text', value: 'On Plan'},
-		{label: 'Scope Status', type: 'text', value: 'On Plan'},
-		{label: 'Budget Status', type: 'text', value: 'Needs Help'},
-		{label: 'Estimated Cost', type: 'text', value: 236300},
-		{label: 'Schedule Status', type: 'text', value: 'Attention'},
-	]
+	status: {
+		name: 'Status',
+		stamp: 'Last Update 7/17 by Robin Sherwood',
+		fields: [
+			{ label: 'Status Update', type: 'longtext', value: 'Completed phase two check point agreed to move to phase three pending the clusure of the current open issues. Planned finish date was adjuested by one week to acoomodate for the end of quarter.' },
+			{label: 'Health', type: 'text', value: 'On Plan'},
+			{label: 'Scope Status', type: 'text', value: 'On Plan'},
+			{label: 'Budget Status', type: 'text', value: 'Needs Help'},
+			{label: 'Estimated Cost', type: 'text', value: 236300},
+			{label: 'Schedule Status', type: 'text', value: 'Attention'},
+		]
+	}
 },
 {
-	name: 'Status',
-	stamp: 'Last Update 7/15 by Herpa Derpa',
-	fields: [
-		{ label: 'Status Update', type: 'longtext', value: 'Herpin all the derps in the land.' },
-		{label: 'Health', type: 'text', value: 'Herpd'},
-		{label: 'Scope Status', type: 'text', value: 'Derpable'},
-		{label: 'Budget Status', type: 'text', value: 'Herpd'},
-		{label: 'Estimated Cost', type: 'text', value: 31337101},
-		{label: 'Schedule Status', type: 'text', value: 'LOL'},
-	]
+	status: {
+		name: 'Status',
+		stamp: 'Last Update 7/15 by Herpa Derpa',
+		fields: [
+			{ label: 'Status Update', type: 'longtext', value: 'Herpin all the derps in the land.' },
+			{label: 'Health', type: 'text', value: 'Herpd'},
+			{label: 'Scope Status', type: 'text', value: 'Derpable'},
+			{label: 'Budget Status', type: 'text', value: 'Herpd'},
+			{label: 'Estimated Cost', type: 'text', value: 31337101},
+			{label: 'Schedule Status', type: 'text', value: 'LOL'},
+		]
+	}
 }];
 
 app.use(express.static(__dirname));
