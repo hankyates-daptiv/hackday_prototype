2196d4b25781c8d627a2bec821c7d562ffa0f411
Hank Yates <hyates@daptiv.com>
2013-08-15 15:47:59 -0700

    whatever

--- js/Prototype.js
+++ js/Prototype.js
@@ -1,39 +1,39 @@
 var myApp = angular.module('myApp', [])
     .controller('dashboard', function ($scope, $http) {
-        $scope.widgets = $http.get('/widgets').then(function (res) {
+        $scope.widgets = $http.get('/status/anaythign').then(function (res) {
             return res.data;
         });
     })
     .directive('ngWidget', function () {
         return {
-                    restrict: 'EACM',
-                    templateUrl: 'widget.html'
-                }
+            restrict: 'EACM',
+            templateUrl: 'widget.html'
+        }
     })
     .directive('ngTablewidget', function () {
         return {
-                    restrict: 'EACM',
-                    templateUrl: 'table-widget.html'
-                }
+            restrict: 'EACM',
+            templateUrl: 'table-widget.html'
+        }
     })
     .directive('ngTextfield', function () {
         return {
-                    restrict: 'EACM',
-                    templateUrl: 'text.html'
-                }
+            restrict: 'EACM',
+            templateUrl: 'text.html'
+        }
     })
     .directive('ngLongtextfield', function () {
         return {
-                    restrict: 'EACM',
-                    templateUrl: 'longtext.html'
-                }
+            restrict: 'EACM',
+            templateUrl: 'longtext.html'
+        }
     })
     .directive('ngDatefield', function () {
         return {
-                    controller: function ($scope) {
-                        $scope.field.date = new Date($scope.field.value).toString();
-                    },
-                    restrict: 'EACM',
-                    templateUrl: 'date.html'
-                }
+            controller: function ($scope) {
+                $scope.field.date = new Date($scope.field.value).toString();
+            },
+            restrict: 'EACM',
+            templateUrl: 'date.html'
+        }
     });
